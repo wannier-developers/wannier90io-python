62c035c9cc393afc4c2cc42f72c780905b78ee9f
Jamal Mustafa <jimustafa@gmail.com>
2022-02-24 23:32:49 -0800

    add test of NNKP parsing

--- tests/test_nnkp.py
+++ tests/test_nnkp.py
@@ -6,11 +6,11 @@ import w90io
 
 
 @pytest.mark.parametrize('example', [f'example{i:02d}' for i in [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 13, 17, 18, 19, 20]])
-def test_parse_win(wannier90, example):
-    with open(pathlib.Path(wannier90)/f'examples/{example}/wannier.win', 'r') as fh:
+def test_parse_nnkp(wannier90, example):
+    with open(pathlib.Path(wannier90)/f'examples/{example}/wannier.nnkp', 'r') as fh:
         contents = fh.read()
 
     try:
-        w90io.parse_win(contents)
+        w90io.parse_nnkp(contents)
     except Exception:
         assert False
