1aaa73233866f5d708237597f33ee6d94d41a587
Jamal Mustafa <jimustafa@gmail.com>
2022-06-18 14:34:36 -0700

    fix typo

--- tests/test_wout.py
+++ tests/test_wout.py
@@ -6,7 +6,7 @@ import w90io
 
 
 @pytest.mark.parametrize('example', [f'example{i:02d}' for i in [1, 2, 3, 4]])
-def test_parse_win(wannier90, example):
+def test_parse_wout(wannier90, example):
     with open(pathlib.Path(wannier90)/f'examples/{example}/wannier.wout', 'r') as fh:
         try:
             w90io.parse_wout_iteration_info(fh)
